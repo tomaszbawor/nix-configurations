873b54d8494b6c30cdc9d448c4414b71ae91cda3
Tomasz Bawor <bawortomasz@gmail.com>
2025-02-15 19:52:27 +0100

    feat: add argocd package

--- hosts/nixosDesktop/configuration.nix
+++ hosts/nixosDesktop/configuration.nix
@@ -1,9 +1,11 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-{ pkgs
-, ...
-}: {
+{
+  pkgs,
+  ...
+}:
+{
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
@@ -68,12 +70,14 @@
     # kubernetes
     kubernetes-helm
     fluxcd
+    argocd
+    argocd-autopilot
     kubectl
     kubectx
     k9s
     lens
 
-    # secret management 
+    # secret management
     sops
     age
     # Gnome
