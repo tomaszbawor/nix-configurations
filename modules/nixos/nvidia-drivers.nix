277d37d15b050c2968ce22eb00c3bb9e0a2a7fd2
Tomasz Bawor <bawortomasz@gmail.com>
2025-03-24 04:33:00 +0100

    Bump drivers

--- modules/nixos/nvidia-drivers.nix
+++ modules/nixos/nvidia-drivers.nix
@@ -27,8 +27,8 @@ in
       #package = config.boot.kernelPackages.nvidiaPackages.beta;
       ## Driver Override example
       package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-        version = "570.86.16";
-        sha256_64bit = "sha256-RWPqS7ZUJH9JEAWlfHLGdqrNlavhaR1xMyzs8lJhy9U=";
+        version = "570.133.07";
+        sha256_64bit = "sha256-LUPmTFgb5e9VTemIixqpADfvbUX1QoTT2dztwI3E3CY=";
         openSha256 = "sha256-PMh5efbSEq7iqEMBr2+VGQYkBG73TGUh6FuDHZhmwHk=";
         settingsSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
         persistencedSha256 = lib.fakeSha256;
