19b1abc95b0b497c17e3e2b38cf10ffd5a708a8e
Tomasz Bawor <bawortomasz@gmail.com>
2025-04-19 11:55:29 +0200

    chore: update deps

--- sync.sh
+++ sync.sh
@@ -9,7 +9,7 @@ git add -A
 commit_msg="chore: update deps"
 
 if ! git diff --cached --quiet; then
-  git commit -m "$(commit_msg)"
+  git commit -m "$commit_msg"
   echo "Commited ..."
 else
   echo "Nothing to commmit - workint tree clean"
