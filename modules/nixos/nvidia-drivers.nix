6372ebecca1714f8243813660b9a388a93b59a42
Tomasz Bawor <bawortomasz@gmail.com>
2025-04-21 20:12:42 +0200

    feat: update nvidia drivers

--- modules/nixos/nvidia-drivers.nix
+++ modules/nixos/nvidia-drivers.nix
@@ -24,8 +24,8 @@ in
       #package = config.boot.kernelPackages.nvidiaPackages.beta;
       ## Driver Override example
       package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-        version = "570.133.07";
-        sha256_64bit = "sha256-LUPmTFgb5e9VTemIixqpADfvbUX1QoTT2dztwI3E3CY=";
+        version = "575.51.02";
+        sha256_64bit = "sha256-XZ0N8ISmoAC8p28DrGHk/YN1rJsInJ2dZNL8O+Tuaa0=";
         openSha256 = "sha256-PMh5efbSEq7iqEMBr2+VGQYkBG73TGUh6FuDHZhmwHk=";
         settingsSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
         persistencedSha256 = lib.fakeSha256;
