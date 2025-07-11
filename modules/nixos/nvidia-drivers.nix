2c11b4d4964e750807d09b1393915fa02585b0d2
Tomasz Bawor <bawortomasz@gmail.com>
2025-07-11 18:50:38 +0200

    chore: update deps

--- modules/nixos/nvidia-drivers.nix
+++ modules/nixos/nvidia-drivers.nix
@@ -17,15 +17,15 @@
     open = false;
     nvidiaSettings = true;
 
-    package = config.boot.kernelPackages.nvidiaPackages.production;
+    #package = config.boot.kernelPackages.nvidiaPackages.production;
     ## Driver Override example
-    #package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-    #  version = "575.51.02";
-    # sha256_64bit = "sha256-XZ0N8ISmoAC8p28DrGHk/YN1rJsInJ2dZNL8O+Tuaa0=";
-    # openSha256 = "sha256-PMh5efbSEq7iqEMBr2+VGQYkBG73TGUh6FuDHZhmwHk=";
-    # settingsSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
-    # persistencedSha256 = lib.fakeSha256;
-    #};
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "575.64.03";
+      sha256_64bit = "sha256-S7eqhgBLLtKZx9QwoGIsXJAyfOOspPbppTHUxB06DKA=";
+      openSha256 = "sha256-PMh5efbSEq7iqEMBr2+VGQYkBG73TGUh6FuDHZhmwHk=";
+      settingsSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
+      persistencedSha256 = lib.fakeSha256;
+    };
   };
 
 }
