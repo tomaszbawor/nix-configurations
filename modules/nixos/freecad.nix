74089b7c496dd493bb8e5dea59a0bc524faa6cf8
Tomasz Bawor <bawortomasz@gmail.com>
2025-08-07 21:58:17 +0200

    chore: update deps

--- modules/nixos/freecad.nix
+++ modules/nixos/freecad.nix
@@ -8,16 +8,4 @@
     freecad-wayland
   ];
 
-  nixpkgs.overlays = [
-    (final: prev: {
-      coin3d = prev.coin3d.overrideAttrs {
-        src = prev.fetchFromGitHub {
-          owner = "coin3d";
-          repo = "coin";
-          rev = "v4.0.3";
-          hash = "sha256-dUFmcUOdNc3ZFtr+Hnh3Q3OY/JA/WxmiRJiU2RFSSus=";
-        };
-      };
-    })
-  ];
 }
