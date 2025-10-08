b10822726a06e672081c4046a2dd8f3fee8f9d9e
Tomasz Bawor <bawortomasz@gmail.com>
2025-10-08 18:25:47 +0200

    small refactor

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -19,9 +19,6 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
-  services.desktopManager.cosmic.enable = true;
-  services.displayManager.cosmic-greeter.enable = true;
-
   # Corne Keyboard
   services.udev.extraRules = ''
     SUBSYSTEM=="usb", ATTR{idVendor}=="4653", ATTR{idProduct}=="0001", MODE="0666"
@@ -47,9 +44,9 @@
   systemd.targets.hibernate.enable = false;
   systemd.targets.hybrid-sleep.enable = false;
 
-  nixpkgs.config.permittedInsecurePackages = [
-    "libsoup-2.74.3"
-  ];
+  # nixpkgs.config.permittedInsecurePackages = [
+  #   "libsoup-2.74.3"
+  # ];
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
--- modules/nixos/xserver.nix
+++ modules/nixos/xserver.nix
@@ -10,5 +10,8 @@
     # windowManager.hypr.enable = true;
   };
 
-  services.displayManager.gdm.enable = true;
+  # services.displayManager.gdm.enable = false;
+
+  services.desktopManager.cosmic.enable = true;
+  services.displayManager.cosmic-greeter.enable = true;
 }
