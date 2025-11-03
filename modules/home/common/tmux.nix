72090ff85a51e6acf295f6598502c61b31905560
Tomasz Bawor <bawortomasz@gmail.com>
2025-11-03 14:28:34 +0100

    tmux

--- modules/home/common/tmux.nix
+++ modules/home/common/tmux.nix
@@ -16,18 +16,32 @@
 
     secureSocket = false;
     historyLimit = 50000;
-    sensibleOnTop = true;
+    # sensibleOnTop = true;
 
     plugins = with pkgs.tmuxPlugins; [
       better-mouse-mode
-      tmux-fzf # Fuzzy Find in TMUX
+      catppuccin
+      cpu
+      battery
       vim-tmux-navigator # Using ctrl-j k l h for switch windows
     ];
 
     extraConfig = ''
       # Terminal setup (Neovim is crying about them)
-      set-option -g default-terminal "screen-256color"
-      set-option -a terminal-features 'XXX:RGB'
+      set-option -g default-terminal "tmux-256color"
+      set-option -g status-position top
+      set -g @catppuccin_flavor "mocha"
+      set -g @catppuccin_window_status_style "rounded"
+
+      # Make the status line pretty and add some modules
+      set -g status-right-length 100
+      set -g status-left-length 100
+      set -g status-left ""
+      set -g status-right "#{E:@catppuccin_status_application}"
+      # set -agF status-right "#{E:@catppuccin_status_cpu}"
+      set -ag status-right "#{E:@catppuccin_status_session}"
+      set -ag status-right "#{E:@catppuccin_status_uptime}"
+      # set -agF status-right "#{E:@catppuccin_status_battery}"
 
       # switch panes using Alt-arrow without prefix
       bind -n M-Left select-pane -L
@@ -44,27 +58,6 @@
       # Intuitive Splits
       bind | split-window -h
       bind - split-window -v
-
-      # --- Status bar ---
-      set -g status-position top
-      set -g status-bg colour235
-      set -g status-fg colour245
-      set -g status-interval 5
-      set -g status-left-length 40
-      set -g status-right-length 120
-
-      set -g status-left "#[fg=colour39,bold]#S #[fg=colour240]| #[fg=colour39]#I:#P"
-      set -g status-right "#[fg=colour244]%Y-%m-%d #[fg=colour245]%H:%M #[fg=colour39]#(whoami)"
-
-      # --- Window title style ---
-      setw -g window-status-current-format " #[fg=colour39,bold]#I:#W#[default] "
-      setw -g window-status-format " #[fg=colour244]#I:#W#[default] "
-
-      set -g status-position top
-      set -g window-status-current-style fg=red,bg=black
-      set -g status-interval 1
-      set -g status-left-length 100
-      set -g status-left ""
     '';
   };
 
