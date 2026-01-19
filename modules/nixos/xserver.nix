c3bc6105a7bba1d6c123a146eea0ad628e29a92c
Tomasz Bawor <bawortomasz@gmail.com>
2026-01-19 15:20:20 +0100

    chore: small udpate

--- modules/nixos/xserver.nix
+++ modules/nixos/xserver.nix
@@ -8,7 +8,12 @@
     };
   };
 
-  # services.displayManager.gdm.enable = false;
+  # Cosmic
   services.desktopManager.cosmic.enable = true;
   services.displayManager.cosmic-greeter.enable = true;
+
+  # KDE
+  # services.desktopManager.plasma6.enable = true;
+  # services.displayManager.sddm.enable = true;
+  # services.displayManager.sddm.wayland.enable = true;
 }
